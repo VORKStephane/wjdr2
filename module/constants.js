5b0bc1091455a61e602424e2b3ab84df1b6b8e7a
VORK Stéphane <vorksprogrammation@gmail.com>
2022-03-25 19:49:07 +0100

    Star Sign

--- module/constants.js
+++ module/constants.js
@@ -175,6 +175,29 @@ wjdr2.hair = {
 	silver : "wjdr2.actors.description.hair.silver",
 };
 
+wjdr2.starSign = {
+	"wymundTheAnachorite" : "wjdr2.actors.description.starSign.wymundTheAnachorite",
+	"theBigCross" : "wjdr2.actors.description.starSign.theBigCross",
+	"theLimnersLine" : "wjdr2.actors.description.starSign.theLimnersLine",
+	"gnuthusTheOx" : "wjdr2.actors.description.starSign.gnuthusTheOx",
+	"dragomasTheDrake" : "wjdr2.actors.description.starSign.dragomasTheDrake",
+	"theGloaming" : "wjdr2.actors.description.starSign.theGloaming",
+	"grungnisBaldric" : "wjdr2.actors.description.starSign.grungnisBaldric",
+	"mammitTheWise" : "wjdr2.actors.description.starSign.mammitTheWise",
+	"mummiTheFool" : "wjdr2.actors.description.starSign.mummiTheFool",
+	"theTwoBullocks" : "wjdr2.actors.description.starSign.theTwoBullocks",
+	"theDancer" : "wjdr2.actors.description.starSign.theDancer",
+	"theDrummer" : "wjdr2.actors.description.starSign.theDrummer",
+	"thePiper" : "wjdr2.actors.description.starSign.thePiper",
+	"vobistTheFaint" : "wjdr2.actors.description.starSign.vobistTheFaint",
+	"theBrokenCart" : "wjdr2.actors.description.starSign.theBrokenCart",
+	"theGreasedGoat" : "wjdr2.actors.description.starSign.theGreasedGoat",
+	"rhyasCauldron" : "wjdr2.actors.description.starSign.rhyasCauldron",
+	"cackelfaxTheCockerel" : "wjdr2.actors.description.starSign.cackelfaxTheCockerel",
+	"theBonesaw" : "wjdr2.actors.description.starSign.theBonesaw",
+	"theWitchlingStar" : "wjdr2.actors.description.starSign.theWitchlingStar"
+};
+
 wjdr2.damageLocation = {
 	head : "wjdr2.actors.damageLocation.head",
 	body : "wjdr2.actors.damageLocation.body",
