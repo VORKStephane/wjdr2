9ba8fe60768ff5c0a6bca4e6231250f38d0467b7
VORK Stéphane <vorksprogrammation@gmail.com>
2022-07-18 10:10:34 +0200

    Standardizing Armor category Object to config

--- module/items/Armor.js
+++ module/items/Armor.js
@@ -1,43 +1,37 @@
+import { prepareConfiguration } from "../DataHandler.js";
 
 const materials = {
     leather : {
-        label: "wjdr2.label.armor.leather",
         category : "light",
         localProtection : 1,
         globalProtection : 1
     },
     studdedLeather : {
-        label : "wjdr2.label.armor.studdedLeather",
         category : "light",
         localProtection : 1,
         globalProtection : 2
     },
     chain : {
-        label : "wjdr2.label.armor.chain",
         category : "medium",
         localProtection : 2,
         globalProtection : 3
     },
     scale : {
-        label : "wjdr2.label.armor.scale",
         category : "medium",
         localProtection : 3,
         globalProtection : 4
     },
     plate : {
-        label : "wjdr2.label.armor.plate",
         category : "heavy",
         localProtection : 2,
         globalProtection : 5
     },
     ithilmar : {
-        label : "wjdr2.label.armor.ithilmar",
         category : "heavy",
         localProtection : 3,
         globalProtection : 4
     },
     gromril : {
-        label : "wjdr2.label.armor.gromril",
         category : "heavy",
         localProtection : 3,
         globalProtection : 6
@@ -63,11 +57,7 @@ function getGlobalProtectionBasedOnMaterial(material) {
 }
 
 export const prepareArmorConfiguration = function() {
-    let toReturn = [];
-    for (let material in materials) {
-        toReturn.push([material, materials[material].label]);
-    }
-    return Object.fromEntries(new Map(toReturn));
+    return prepareConfiguration(materials, "wjdr2.label.armor.");
 }
     
 export class Armor {
